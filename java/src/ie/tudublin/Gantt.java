55a28efc80a98763b827508d613ca21a1486ebcb
Kelly-001 <mankieu_01@yahoo.ie>
2020-04-22 17:47:17 +0100

    displaying the tasks

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -33,6 +33,43 @@ public class Gantt extends PApplet
 			System.out.println(t);
 		}	
 	}
+
+	public void displayTasks()
+	{
+		float gap = 150;
+		float border = (float) (height * 0.05);
+		int days= 31;
+		int p;
+		int q;
+		float col;
+		stroke(255);
+		for(int i=1;i<days;i++)
+		{
+			float x = map(i,1,30,gap,width-border);
+			line(x, border, x, height - border);
+			textAlign(CENTER);
+			text(i, x, 15);
+		}
+		for(int i=0;i<task.size();i++)
+		{
+			Task t = task.get(i);
+			p = t.getStart();
+			q = t.getEnd();
+			q = q - p;
+			noStroke();
+			col = map(i,0,11,0,330);
+			float start = map(p,1,30,gap,width - border);
+			float end = map(q,0,30,0,width - gap - border);
+			float y = map(i,0,task.size(),border +20 , height - gap);
+			fill(col,255,255);
+			rect(start,y,end,30);
+			fill(255);
+			text(t.getTask(),75,y+15);
+		}
+
+		
+
+	}
 	
 	public void mousePressed()
 	{
@@ -50,10 +87,12 @@ public class Gantt extends PApplet
 	{
 		loadTasks();
 		printTasks();
+		colorMode(HSB);
 	}
 	
 	public void draw()
 	{			
 		background(0);
+		displayTasks();
 	}
 }
